d634ce6badcfb1149563fa55d5350bfb83db27ab
Vaibhav Khokha <vaibhavkhokha@gmail.com>
2026-07-21 22:20:01 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {
+        if(height.empty())
+        {return 0;}
+
+        vector<int> maxLeft;
+        maxLeft.reserve(height.size());
+
+        vector<int> maxRight(height.size(), 0);
+
+        int ml = height[0];
+        int mr = height.back();
+
+        maxLeft.emplace_back(ml);
+
+        for(int i = 1; i<height.size(); i++)
+        {
+            ml = max(ml, height[i]);
+            maxLeft.emplace_back(ml);
+        }
+
+        maxRight.back() = mr;
+
+        for(int i = maxRight.size() - 2; i>=0; i--)
+        {
+            mr = max(mr, height[i]);
+
+            maxRight[i] = mr;
+        }
+
+        vector<int> minLR;
+        minLR.reserve(height.size());
+
+        for(int i = 0; i<maxLeft.size(); i++)
+        {
+            if(min(maxLeft[i], maxRight[i]) - height[i] >= 0)
+            {
+                minLR.emplace_back(min(maxLeft[i], maxRight[i]) - height[i]);
+            }
+
+        }
+
+        int result = 0;
+
+        for(int num : minLR)
+        {
+            result += num;
+        }
+
+        return result;
+    }
+
+};
